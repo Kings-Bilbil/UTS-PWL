5f965779f2cd5b4c18cea82336ea6bf5aee2540c
Kings-Bilbil <nabilnur556@gmail.com>
2026-04-05 03:59:55 +0000

    Commit 2

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,9 +1,16 @@
-import { render } from "../config/viewEngine";
+import { render } from "../config/viewEngine.js";
+import * as model from "../models/mahasiswaModel.js";
 
 export const home = async (c) => {
+  // 1. Ambil semua data mahasiswa dari database melalui model
+  const data = await model.getAll();
+  
+  // 2. Kirim variabel yang dibutuhkan oleh home.ejs
   const html = await render("home", {
     title: "Dashboard Bun MVC",
-    message: "Hello dari Bun + Tailwind",
-  });
+    mahasiswa: data, // Untuk tabel di dashboard
+    totalMahasiswa: data.length // Untuk angka total di card dashboard
+  }, c);
+  
   return c.html(html);
 };
